d118bc998031ab354792adbc45d4d1c376211516
Rizal Solihin <ripisopol@gmail.com>
2026-04-25 23:42:47 +0700

    feat: add persistent volume for SQLite

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./notes.db"
+DATABASE_URL = "sqlite:////app/data/notes.db"
 
 engine = create_engine(
     DATABASE_URL,
