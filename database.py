c58b90efb9f4dd0c71dba132c97d1bea14417987
Rizal Solihin <ripisopol@gmail.com>
2026-04-26 06:37:09 +0700

    feat: persistent volume, responsive layout

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:////app/data/notes.db"
+DB_PATH = os.getenv("DB_PATH", "./notes.db")
+DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     DATABASE_URL,
